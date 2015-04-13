3ff09a0c2e442066216c15109b345a2c38fbb514
ka <ka.kaosf@gmail.com>
2015-04-14 02:02:24 +0900

    Replace greet.html from static to dynamic for anti-forgery
    ref.
      https://github.com/ring-clojure/ring-anti-forgery
      https://forums.pragprog.com/forums/308/topics/13009

--- src/greet/handler.clj
+++ src/greet/handler.clj
@@ -1,10 +1,37 @@
 (ns greet.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [ring.util.anti-forgery :refer [anti-forgery-field]]))
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
+           (GET "/greet.html" []
+                (str
+"<!DOCTYPE html>
+ <html>
+   <head>
+   </head>
+   <body>
+     <form method=\"post\" action=\"post_name\">
+       <table>
+         <tr>
+           <td>First Name:</td>
+           <td><input name=\"first_name\" type=\"text\" value=\"\" /></td>
+         </tr>
+         <tr>
+           <td>Last Name:</td>
+           <td><input name=\"last_name\" type=\"text\" value=\"\" /></td>
+         </tr>
+       </table>
+       <div><input id=\"greet_button\" type=\"submit\" value=\"Greet\" /></div>
+"
+(anti-forgery-field)
+"
+       </form>
+     <div id=\"greeting_area\"></div><br/>
+   </body>
+ </html>"))
   (POST "/post_name" [first_name last_name]
         (format "Hello, %s %s" first_name last_name))
   (route/not-found "Not Found"))
